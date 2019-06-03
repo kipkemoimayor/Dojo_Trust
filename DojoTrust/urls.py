bef463438fca50a3aaba8e320c55e559c04a022a
kipkemoimayor <collinskipkemoi24@gmail.com>
2019-06-03 15:49:35 +0300

    Created an aunthentication system

--- DojoTrust/urls.py
+++ DojoTrust/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import url,include
 from django.contrib import admin
+from django.contrib.auth import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^',include('dojotrust.urls')),
+    url(r'^accounts/',include('registration.backends.simple.urls'))
 ]
