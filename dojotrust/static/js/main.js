49e2721c3bc172bcbfe0027e1d4957836d3cd70e
kipkemoimayor <collinskipkemoi24@gmail.com>
2019-06-05 16:03:06 +0300

    Created a flex boz

--- dojotrust/static/js/main.js
+++ dojotrust/static/js/main.js
@@ -10,35 +10,5 @@ $(document).ready(function () {
     '<ul  class="list-group"> <li class="list-group-item"><a class="ref" href="">Cancel</a> </li></ul>',
   })
 })
-// $(".follow").click(function() {
-//   $(".following").show()
-//   $(".follow").hide()
-//
-//
-// })
-// $(".following").click(function() {
-//   $(".follow").show()
-//   $(".following").hide()
-//
-//
-// })
-
 
 })
-
-// $(document).ready(function(){
-//   $("#prompt").click(function(){
-//     swal({
-//       button:false,
-//       text:"heu",
-//       className:'red-bg',
-//       closeOnEsc:true,
-//       '<div>
-//       <a href="">Login<a>
-//       </div>'
-//
-//
-//     })
-//
-//   })
-// })
